eeb301facf5d52b9f211246eb52a8132c6ab02af
Cuong Nguyen <n_cuong@hotmail.com>
2015-04-14 18:09:35 +0700

    db script

--- dbscripts/CoffeeAPP.sql
+++ dbscripts/CoffeeAPP.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v9.20 
-MySQL - 5.6.23-log : Database - coffeeapp
+MySQL - 5.5.16 : Database - coffeeapp
 *********************************************************************
 */
 
@@ -24,20 +24,17 @@ DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee` (
   `EMPLOYEE_ID` int(11) NOT NULL AUTO_INCREMENT,
   `EMPLOYEE_NAME` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `EMPLOYEE_CITY` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `EMPLOYEE_ADDRESS` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `EMPLOYEE_TELEPHONE` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `SHIFT` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `WORK_DATE` datetime DEFAULT NULL,
   `EMPLOYEE_SEX` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
   `USER_NAME` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `PASSWORD` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`EMPLOYEE_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `employee` */
 
-insert  into `employee`(`EMPLOYEE_ID`,`EMPLOYEE_NAME`,`EMPLOYEE_CITY`,`EMPLOYEE_ADDRESS`,`EMPLOYEE_TELEPHONE`,`SHIFT`,`WORK_DATE`,`EMPLOYEE_SEX`,`USER_NAME`,`PASSWORD`) values (1,'Cuong Nguyen','Danang','Viet Nam','123456','1','2015-04-08 11:34:59','Nam','cuongn','123456'),(2,'b','bb','bbb','1','b',NULL,'male','b','123'),(3,'c','cc','ccc','c','cc',NULL,'c','c','123'),(10,'fsadf','dfsadf','dfsdf','fasdf','adfas',NULL,'asdf','asdf','asdfsdf');
+insert  into `employee`(`EMPLOYEE_ID`,`EMPLOYEE_NAME`,`EMPLOYEE_ADDRESS`,`EMPLOYEE_TELEPHONE`,`EMPLOYEE_SEX`,`USER_NAME`,`PASSWORD`) values (1,'Cuong Nguyen','Da nang','0987612415','Nam','c','1'),(3,'Nguyen Thuy Nguyen','Da nang','0985698745','Nam','nguyen','1'),(5,'Vo Dinh Hieu','Da nang','098541234','Nam','h','123456'),(7,'Hoang Thuy Linh','Hanoi','0123456789','Nữ','linhht','1');
 
 /*Table structure for table `orderdetail` */
 
@@ -46,7 +43,6 @@ DROP TABLE IF EXISTS `orderdetail`;
 CREATE TABLE `orderdetail` (
   `ORDER_ID` int(11) DEFAULT NULL,
   `PRODUCT_ID` int(11) unsigned DEFAULT NULL,
-  `PAYMENT` int(11) unsigned DEFAULT NULL,
   `QUANTITY` int(11) unsigned DEFAULT NULL,
   `ORDERDETAIL_ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ORDERDETAIL_ID`),
@@ -58,7 +54,7 @@ CREATE TABLE `orderdetail` (
 
 /*Data for the table `orderdetail` */
 
-insert  into `orderdetail`(`ORDER_ID`,`PRODUCT_ID`,`PAYMENT`,`QUANTITY`,`ORDERDETAIL_ID`) values (1,1,12000,2,1);
+insert  into `orderdetail`(`ORDER_ID`,`PRODUCT_ID`,`QUANTITY`,`ORDERDETAIL_ID`) values (1,1,2,1);
 
 /*Table structure for table `orders` */
 
@@ -66,21 +62,16 @@ DROP TABLE IF EXISTS `orders`;
 
 CREATE TABLE `orders` (
   `ORDER_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PRODUCT_ID` int(11) DEFAULT NULL,
-  `DATE_PAYMENT` datetime DEFAULT NULL,
-  `EMPLOYEE_ID` int(11) DEFAULT NULL,
+  `DATE_PAYMENT` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `TABLE_ID` int(11) DEFAULT NULL,
   PRIMARY KEY (`ORDER_ID`),
-  KEY `FK_order` (`PRODUCT_ID`),
-  KEY `FK_order_employee` (`EMPLOYEE_ID`),
   KEY `FK_tables` (`TABLE_ID`),
-  CONSTRAINT `FK_order_employee` FOREIGN KEY (`EMPLOYEE_ID`) REFERENCES `employee` (`EMPLOYEE_ID`) ON DELETE SET NULL,
   CONSTRAINT `FK_order_tables` FOREIGN KEY (`TABLE_ID`) REFERENCES `tables` (`TABLE_ID`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `orders` */
 
-insert  into `orders`(`ORDER_ID`,`PRODUCT_ID`,`DATE_PAYMENT`,`EMPLOYEE_ID`,`TABLE_ID`) values (1,1,'2015-04-03 09:37:11',1,1);
+insert  into `orders`(`ORDER_ID`,`DATE_PAYMENT`,`TABLE_ID`) values (1,'2015-04-03 09:37:11',1),(2,'2015-04-14 18:02:06',1);
 
 /*Table structure for table `product` */
 
@@ -92,11 +83,11 @@ CREATE TABLE `product` (
   `PRODUCT_QUANTITY` int(11) unsigned DEFAULT NULL,
   `PRODUCT_PRICE` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`PRODUCT_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `product` */
 
-insert  into `product`(`PRODUCT_ID`,`PRODUCT_NAME`,`PRODUCT_QUANTITY`,`PRODUCT_PRICE`) values (1,'cafeDen',100,6000),(2,'cafeSua',110,7000),(3,'stingDau',80,10000),(4,'stingVang',50,10000),(5,'pepsi',80,11000),(6,'coca',75,11000),(7,'redbull',11,15000),(8,'duty',48,12000),(9,'c2',39,14000),(10,'khoang',61,50000);
+insert  into `product`(`PRODUCT_ID`,`PRODUCT_NAME`,`PRODUCT_QUANTITY`,`PRODUCT_PRICE`) values (1,'Cafe Đen',100,5000),(2,'Cafe Sữa',100,6000),(3,'Sting Vàng',100,6000),(4,'Sting Dâu',50,6000),(5,'Pepsi',80,7000),(6,'Coca Cola',75,7000),(7,'Red Bull',100,12000),(9,'C2',50,8000),(10,'Nước khoáng lạc',61,6000),(11,'Nước khoáng ngọt',100,6000),(12,'Trà gừng',50,8000),(13,'Nước chanh',15,7000),(14,'adad',15,15000),(16,'sd12',12,15000),(17,'Yomost',12,10000),(18,'Sữa đậu nành',50,6000),(19,'Nước mía',10,5000);
 
 /*Table structure for table `tables` */
 
@@ -108,11 +99,11 @@ CREATE TABLE `tables` (
   `TABLE_QUANTITY` int(11) unsigned DEFAULT NULL,
   `TABLE_POSITION` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`TABLE_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `tables` */
 
-insert  into `tables`(`TABLE_ID`,`TABLE_NAME`,`TABLE_QUANTITY`,`TABLE_POSITION`) values (1,'table 1',10,'1');
+insert  into `tables`(`TABLE_ID`,`TABLE_NAME`,`TABLE_QUANTITY`,`TABLE_POSITION`) values (1,'Bàn 1',10,'1'),(2,'Bàn 2',NULL,NULL),(5,'Bàn 3',NULL,NULL),(6,'Bàn 4',NULL,NULL),(7,'Bàn 5',NULL,NULL),(8,'Bàn 6',NULL,NULL),(9,'Bàn 7',NULL,NULL),(10,'Bàn 8',NULL,NULL),(11,'Bàn 9',NULL,NULL),(12,'Bàn 10',NULL,NULL),(13,'Bàn 11',NULL,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
